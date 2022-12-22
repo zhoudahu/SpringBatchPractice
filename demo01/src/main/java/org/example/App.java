eb520431a9cd17f94c3690d32dcfe3283775fc2d
zhoudahu <1660218706@qq.com>
2022-12-22 20:43:57 +0800

    初次提交

--- demo01/src/main/java/org/example/App.java
+++ demo01/src/main/java/org/example/App.java
@@ -14,7 +14,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        SpringApplication.run(App.class, args);
+        aaaaaaaaaadsdasdsadsadsadsaSpringApplication.run(App.class, args);
     }
 
 
